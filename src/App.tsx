7d54c08e84a3c5e2a632c73fb4ce3d7ea3850fa0
batorlancs <batorgergely3@gmail.com>
2024-12-10 22:47:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+    return (
+        <div className="flex flex-col bg-slate-200 items-center justify-center h-screen">
+            <Button>Click me</Button>
+        </div>
+    );
+}
+
+export default App;
